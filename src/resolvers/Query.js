712eda7212eea5d3d4f2be28cd46fc6153efce64
Aldo Dominguez <nonexistant@outlook.com>
2021-02-21 14:02:15 -0600

    Added authentication for the queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,5 @@
+import getUserId from './../utils/getUserId'
+
 const Query = {
     users(parent, args, { prisma }, info){
 
@@ -26,19 +28,31 @@ const Query = {
 
         if(args.query)
             opArgs.where = {
-                OR : [
-                    {
-                        title : {
-                            contains : args.query
-                        }
+                AND : [
+                    {   
+                        published : true
                     },
                     {
-                        body : {
-                            contains : args.query
-                        }
+                        OR : [
+                            {
+                                title : {
+                                    contains : args.query
+                                }
+                            },
+                            {
+                                body : {
+                                    contains : args.query
+                                }
+                            }
+                        ]
                     }
                 ]
             }
+        else 
+            opArgs.where = {
+                published : true
+            }
+        
         return prisma.post.findMany(opArgs)
     },
     comments(parent, args, { prisma }, info){
@@ -52,6 +66,111 @@ const Query = {
                 }
             }
         return prisma.comment.findMany(opArgs)
+    },
+
+    async post(parent, args, { prisma, request }, info) {
+        const userId = getUserId(request, false)
+
+        let post = undefined
+
+        if(userId)
+            post = await prisma.post.findFirst({
+                where : {
+                    AND : [
+                        {
+                            id : args.id
+                        },
+                        {
+                            OR : [
+                                {
+                                    published : true
+                                },
+                                {
+                                    authorId : userId
+                                }
+                            ]
+                        }
+                    ]
+                }
+            })
+        else {
+            post = await prisma.post.findFirst({
+                where : {
+                    AND : [
+                        {
+                            id : args.id
+                        },
+                        {
+                            published : true
+                        }
+                    ]
+                }
+            })
+        }
+            
+
+        if(!post)
+            throw new Error('Post not found.')
+        return post
+
+    },
+
+    async me(parent, args, { prisma, request }) {
+
+        const userId = getUserId(request, true)
+
+        const user = await prisma.user.findUnique({
+            where : {
+                id : userId
+            }
+        })
+
+        if(!user)
+            throw new Error('The user was not found.')
+        
+        return user
+
+    },
+
+    async myPosts(parent, args, { prisma, request }, info) {
+        
+        const userId = getUserId(request)
+
+        const opArgs = {}
+
+        if(args.query)
+            opArgs.where = {
+                AND : [
+                    {   
+                        authorId : userId
+                    },
+                    {
+                        OR : [
+                            {
+                                title : {
+                                    contains : args.query
+                                }
+                            },
+                            {
+                                body : {
+                                    contains : args.query
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        else    
+            opArgs.where = {
+                authorId : userId
+            }
+
+        const posts = await prisma.post.findMany(opArgs)
+
+        if(posts.length == 0)
+            throw new Error('No posts were found related to the provided user.')
+        return posts
+
     }
 }
 
